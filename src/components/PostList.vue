ef338e5225e7a9bd5020cfd92e8fc7522ca6dad4
wjdrbd96 <wjdrbs966@naver.com>
2022-09-27 00:09:25 +0900

    Post List 뼈대 구성

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,43 @@
 <template>
-  <div id="app">
-    Hi HIHIHIHI
+  <div class="container-fluid">
+    <div class="row mt-3">
+      <h1>Post List</h1>
+      <div class="row mt-2">
+        <div class="col-md-12">
+          <div class="table-responsive">
+            <table class="table table-sm table-hover word-break" style="table-layout: fixed">
+              <thead class="table-secondary">
+                <tr class="text-center">
+                  <th class="col-1">
+                    #
+                  </th>
+                  <th class="col-1">
+                    name
+                  </th>
+                  <th class="col-1">
+                    title
+                  </th>
+                </tr>
+              </thead>
+              <tbody class="text-center">
+                <template>
+                  <tr>
+                    <td>test1</td>
+                    <td>test2</td>
+                    <td>test5</td>
+                  </tr>
+                  <tr>
+                    <td>test3</td>
+                    <td>test4</td>
+                    <td>test6</td>
+                  </tr>
+                </template>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,6 +48,10 @@ export default {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+
+  methods: {
+    
   }
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,7 @@ const router = new VueRouter({
   mode: 'history',
   routes: [
     { 
-      path: '/test', 
+      path: '/posts', 
       component: PostList
     },
   ]
